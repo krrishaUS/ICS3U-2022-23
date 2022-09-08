aceac7008541119db8918daf13273374e24b4d61
Krrisha Udaya Shankar <kshankar@bayviewglen.ca>
2022-09-08 16:35:40 -0400

    Spelling mistake

--- Unit One - Introduction to Java/src/day2/ExampleOne.java
+++ Unit One - Introduction to Java/src/day2/ExampleOne.java
@@ -4,7 +4,7 @@ package day2;
  * This is a block commment that spans more than one line (usually)
  * 
  * The class is callend ExampleOne (same name as the file)
- * One of the classes in the project must hace a main method (function) that tells java where to start running 
+ * One of the classes in the project must have a main method (function) that tells java where to start running 
  */
 public class ExampleOne {
 public static void main(String[] args) {
@@ -19,7 +19,7 @@ public static void main(String[] args) {
     System.out.println("B");
     System.out.println("C");
     // prinrln adds a new line after printing 
-    // print leaves the cursor where it was afer prints 
+    // print leaves the cursor where it was after printing
     System.out.println("E");
     System.out.println("F");
     System.out.println("G");
