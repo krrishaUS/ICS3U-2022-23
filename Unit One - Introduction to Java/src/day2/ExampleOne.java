1c71a8ba0b6656bd3028209b6cb0b4a5092e3373
Krrisha Udaya Shankar <kshankar@bayviewglen.ca>
2022-09-08 14:47:41 -0400

    Initial Commit

---
+++ Unit One - Introduction to Java/src/day2/ExampleOne.java
@@ -0,0 +1,27 @@
+package day2;
+
+/**
+ * This is a block commment that spans more than one line (usually)
+ * 
+ * The class is callend ExampleOne (same name as the file)
+ * One of the classes in the project must hace a main method (function) that tells java where to start running 
+ */
+public class ExampleOne {
+public static void main(String[] args) {
+    System.out.println("Welcome to AP Computer Science");
+    System.out.println("Welcome to AP Computer Science");
+    System.out.println("Welcome to AP Computer Science");
+    System.out.println();
+    System.out.println();
+
+
+    System.out.println("A");
+    System.out.println("B");
+    System.out.println("C");
+    // prinrln adds a new line after printing 
+    // print leaves the cursor where it was afer prints 
+    System.out.println("E");
+    System.out.println("F");
+}
+    
+}
---
+++ Unit One - Introduction to Java/src/day2/ExampleTwo.java
@@ -0,0 +1,29 @@
+package day2;
+
+/**
+ * Example about Escape Sequences.
+ * 
+ * Naming Conventions:
+ * folders (packages): all lower case with no spaces
+ * Classes: UpperCase on first letter of each word (Pascal Case)
+ * methods/fucntions (variables): camelCase all lower case except the first letter of each word except the first word.
+ * 
+ * Constants: ALL upper case _ to distinquish words NUM_STUDENTS
+ */
+public class ExampleTwo {
+    public static void main(String[] args) {
+
+        // We must escape the quote inside the round brackets because quotes have meaning in Java
+        // \ backslash is used to escape characters 
+        System.out.println("Computer Science is the \"Best\"Course Ever");
+
+        // There are a small subset of characters that can be escaped.
+        // BUT the AP exam will only test you on 3 of them \\ \" \n
+
+        // to display a backslash you must escape it \\
+        System.out.println("This is \\\\a test");
+
+        System.out.println("ABCD\nEFGHIJK");
+    }
+    
+}
