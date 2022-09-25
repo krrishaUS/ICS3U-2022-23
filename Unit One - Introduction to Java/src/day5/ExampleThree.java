aae4f3197e35c4b2b0fff70560be3da1e1788b74
Krrisha Udaya Shankar <kshankar@bayviewglen.ca>
2022-09-25 18:33:13 -0400

    edit

--- Unit One - Introduction to Java/src/day5/ExampleThree.java
+++ Unit One - Introduction to Java/src/day5/ExampleThree.java
@@ -4,6 +4,8 @@ public class ExampleThree {
     public static void main(String[] args) {
         double x =  5 / 0;
 
+        System.out.println("This is an arithmetic exception: " + x);
+
         // need to know Arithmetic Exceptions for AP Exam
     }
     
